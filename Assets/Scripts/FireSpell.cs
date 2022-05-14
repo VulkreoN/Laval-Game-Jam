24cea2176b93d4af3aec150395d0d7711bcb6b1d
VulkreoN <hakim.redjem@epitech.eu>
2022-05-14 20:42:24 +0200

    [ADD] FIREBALLS

--- Assets/Scripts/FireSpell.cs
+++ Assets/Scripts/FireSpell.cs
@@ -7,15 +7,27 @@ public class FireSpell : MonoBehaviour
 {
     public GameObject projectile;
     public float speed = 300f;
+    public Transform Head;
+
+    public void Cast()
+    {
+        Debug.Log("testing...");
+        try
+        {
+            GameObject ball = Instantiate(projectile, Head.position, Head.rotation);
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, speed));
+            Destroy(ball, 10);
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
 
     void Start()
     {
-        GameObject ball = Instantiate(projectile, transform.position,
-                                                     transform.rotation);
-        ball.SetActive(true);
-        ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3
-                                            (0, 0, speed));
-        Destroy(ball, 10);
+       //
     }
 
     void Update()
